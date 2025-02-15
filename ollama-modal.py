5a6389c4f85455b6db03261ba0a4b4138ae87c69
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2025-02-15 19:00:25 +0200

    add fastapi

--- ollama-modal.py
+++ ollama-modal.py
@@ -1,11 +1,15 @@
-import modal
 import os
 import subprocess
 import time
+from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import StreamingResponse
+from starlette.background import BackgroundTask
 
-from modal import build, enter, method
+from modal import Image, App, enter, asgi_app
+import httpx
 
-MODEL = os.environ.get("MODEL", "llama3:instruct")
+MODEL = os.environ.get("MODEL", "deepscaler")
 
 
 def pull(model: str = MODEL):
@@ -17,7 +21,7 @@ def pull(model: str = MODEL):
 
 
 image = (
-    modal.Image.debian_slim()
+    Image.debian_slim()
     .apt_install("curl", "systemctl")
     .run_commands(  # from https://github.com/ollama/ollama/blob/main/docs/linux.md
         "curl -L https://ollama.com/download/ollama-linux-amd64.tgz -o ollama-linux-amd64.tgz",
@@ -25,56 +29,41 @@ image = (
         "useradd -r -s /bin/false -U -m -d /usr/share/ollama ollama",
         "usermod -a -G ollama $(whoami)",
     )
-    .copy_local_file("ollama.service", "/etc/systemd/system/ollama.service")
-    .pip_install("ollama")
+    .add_local_file("ollama.service", "/etc/systemd/system/ollama.service", copy=True)
+    .pip_install("fastapi[standard]","ollama", "httpx", "starlette")
     .run_function(pull)
 )
 
-app = modal.App(name="ollama", image=image)
+app = App(name="ollama", image=image)
+api = FastAPI()
+client = httpx.AsyncClient(base_url="http://localhost:11434/api", timeout=httpx.Timeout(10.0))
 
-with image.imports():
-    import ollama
+@api.get("/api/{path:path}")
+async def tile_request(path: str):
+    req = client.build_request("GET", path)
+    r = await client.send(req, stream=True)
+    return StreamingResponse(
+        r.aiter_raw(),
+        background=BackgroundTask(r.aclose),
+        headers=r.headers
+   )
+   
+@api.post("/api/{path:path}")
+async def tile_request(path: str, request: Request):
+    req = client.build_request("POST", path, data=await request.body())
+    r = await client.send(req, stream=True)
+    return StreamingResponse(
+        r.aiter_raw(),
+        background=BackgroundTask(r.aclose),
+        headers=r.headers
+    )
 
-
-@app.cls(gpu="a10g", container_idle_timeout=300)
+@app.cls(gpu="T4",container_idle_timeout=60,)
 class Ollama:
-    @build()
-    def pull(self):
-        # TODO(irfansharif): Was hoping that the following would use an image
-        # with this explicit @build() step's results, but alas, it doesn't - so
-        # we're baking it directly into the base image above. Also, would be
-        # nice to simply specify the class name? Not like the method is
-        # specified has any relevance.
-        #
-        #  $ modal shell ollama-modal.py::Ollama.infer
-
-        # pull(model=MODEL)
-        ...
-
     @enter()
     def load(self):
         subprocess.run(["systemctl", "start", "ollama"])
 
-    @method()
-    def infer(self, text: str, verbose: bool = False):
-        stream = ollama.chat(
-            model=MODEL, messages=[{"role": "user", "content": text}], stream=True
-        )
-        for chunk in stream:
-            yield chunk["message"]["content"]
-            if verbose:
-                print(chunk["message"]["content"], end="", flush=True)
-        return
-
-
-# Convenience thing, to run using:
-#
-#  $ modal run ollama-modal.py [--lookup] [--text "Why is the sky blue?"]
-@app.local_entrypoint()
-def main(text: str = "Why is the sky blue?", lookup: bool = False):
-    if lookup:
-        ollama = modal.Cls.lookup("ollama", "Ollama")
-    else:
-        ollama = Ollama()
-    for chunk in ollama.infer.remote_gen(text, verbose=False):
-        print(chunk, end="", flush=False)
+    @asgi_app()
+    def serve(self):
+        return api
