fdeaf4b3cd2a78545041aaaeb4c7c3262737e390
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2025-02-20 17:00:48 +0200

    add persistent models volume

--- ollama-modal.py
+++ ollama-modal.py
@@ -1,24 +1,14 @@
-import os
 import subprocess
-import time
 from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.responses import StreamingResponse
 from starlette.background import BackgroundTask
 
-from modal import Image, App, enter, asgi_app
+from modal import Image, App, Volume, enter, asgi_app
 import httpx
 
-MODEL = os.environ.get("MODEL", "deepscaler")
-
-
-def pull(model: str = MODEL):
-    subprocess.run(["systemctl", "daemon-reload"])
-    subprocess.run(["systemctl", "enable", "ollama"])
-    subprocess.run(["systemctl", "start", "ollama"])
-    time.sleep(2)  # 2s, wait for the service to start
-    subprocess.run(["ollama", "pull", model], stdout=subprocess.PIPE)
-
+MODELS_DOWNLOAD_PATH = "/root/models"
+volume = Volume.from_name("ollama", create_if_missing=True)
 
 image = (
     Image.debian_slim()
@@ -31,15 +21,16 @@ image = (
     )
     .add_local_file("ollama.service", "/etc/systemd/system/ollama.service", copy=True)
     .pip_install("fastapi[standard]","ollama", "httpx", "starlette")
-    .run_function(pull)
+    .env({"OLLAMA_MODELS": MODELS_DOWNLOAD_PATH})
+    .run_commands(["systemctl daemon-reload", "systemctl enable ollama"])
 )
 
-app = App(name="ollama", image=image)
+app = App(name="ollama", image=image, volumes={MODELS_DOWNLOAD_PATH: volume})
 api = FastAPI()
 client = httpx.AsyncClient(base_url="http://localhost:11434/api", timeout=httpx.Timeout(10.0))
 
 @api.get("/api/{path:path}")
-async def tile_request(path: str):
+async def proxy_request(path: str):
     req = client.build_request("GET", path)
     r = await client.send(req, stream=True)
     return StreamingResponse(
@@ -49,7 +40,7 @@ async def tile_request(path: str):
    )
    
 @api.post("/api/{path:path}")
-async def tile_request(path: str, request: Request):
+async def proxy_request(path: str, request: Request):
     req = client.build_request("POST", path, data=await request.body())
     r = await client.send(req, stream=True)
     return StreamingResponse(
